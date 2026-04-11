05f785393a7bafe4e27760328272ea7afae00c22
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-04-11 16:42:50 +0500

    Added basic word filtering and a simple hello command

--- main.py
+++ main.py
@@ -14,3 +14,27 @@ intents.members = True
 
 bot = commands.Bot(command_prefix="!", intents=intents)
 
+@bot.event
+async def on_ready():
+    print("ready")
+
+@bot.event
+async def on_member_join(member):
+    await member.send(f"Welcome to the server {member.name}")       #to send them the message privatly
+
+@bot.event
+async def on_message(message): # it will trigger for any message
+    if message.author == bot.user:
+        return
+    
+    if "shit" in message.content.lower():
+        await message.delete()
+        await message.channel.send(f"{message.author.mention} Do not use that word")
+    
+    await bot.process_commands(message) # allows continuing handling all the message we want it to process continue message
+
+@bot.command() # used to create the custom commands now whenever we will do !hello it will send hello name
+async def hello(ctx):
+    await ctx.send(f"Hello {ctx.author.mention} !")
+
+bot.run(token,log_handler=handler,log_level=logging.DEBUG)
